f9bd82f9dc6bcc3c127e929456e2f414bd750ba8
ranxiaolong <rxl5555555@qq.com>
2017-11-16 00:38:28 +0800

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS tbl_test;
+
+CREATE TABLE tbl_test(id INT NOT NULL DEFAULT 1, name text, PRIMARY KEY(id));
+INSERT INTO tbl_test VALUES (2, "hello");
+UPDATE tbl_test SET name = "abc" where id = 2;
